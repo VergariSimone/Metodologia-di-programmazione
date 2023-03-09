fa9bc33801237675ff784112d9db43d5e094061f
Simone Vergari <94738179+VergariSimone@users.noreply.github.com>
2023-03-09 22:16:10 +0100

    Add files via upload

---
+++ StringEs1.java
@@ -0,0 +1,15 @@
+
+public class StringEs1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s = "Mississippi";
+		
+		s = s.replace("i", "ii");
+		s = s.replace("ss","s");
+		System.out.println(s);
+		
+	}
+
+}
